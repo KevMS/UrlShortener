62e039c3a6538a5dc6fe941f8044fda94ac93aea
Kevin Saludaga <111947512+KevMS@users.noreply.github.com>
2024-08-25 22:40:03 +0800

    update

--- client/url-shortner-app/src/components/FormContainer/FormContainer.tsx
+++ client/url-shortner-app/src/components/FormContainer/FormContainer.tsx
@@ -9,6 +9,7 @@ interface IFormContainerProps {
 const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
   const { updateReloadState } = props;
   const [fullUrl, setFullUrl] = React.useState<string>("");
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
@@ -21,32 +22,33 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
       console.log(error);
     }
   };
+
   return (
-    <div className="container mx-auto p-2">
-      <div className="bg-banner my-8 rounded-xl bg-cover bg-center">
-        <div className="w-full h-full rounded-xl p-20 backdrop-brightness-50">
-          <h1 className=" text-white text-4xl text-center pb-4">
+    <div className="container mx-auto p-4 sm:p-6 lg:p-8">
+      <div className="bg-banner my-4 sm:my-8 rounded-xl bg-cover bg-center">
+        <div className="w-full h-full rounded-xl p-4 sm:p-8 lg:p-12 backdrop-brightness-50">
+          <h1 className="text-white text-2xl sm:text-3xl md:text-4xl text-center pb-4">
             URL Shortener
           </h1>
-          <p className=" text-white text-center pb-2 text-xl font-extralight">
+          <p className="text-white text-center pb-2 text-base sm:text-lg font-light">
             Paste Your Untidy Link Here
           </p>
-          <p className=" text-white text-center pb-4 text-sm font-thin">
+          <p className="text-white text-center pb-4 text-sm sm:text-base font-thin">
             Easily shorten long links with our free tool. Create clean,
             shareable URLs in seconds, perfect for social media, emails, and
             more. Streamline your links effortlessly!
           </p>
           <form onSubmit={handleSubmit}>
-            <div className="flex">
+            <div className="flex flex-col sm:flex-row items-center">
               <div className="relative w-full">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-slate-800">
                   urlshortener.link /
                 </div>
                 <input
                   type="text"
                   placeholder="Add your link"
                   required
-                  className="block w-full p-4 ps-36 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 "
+                  className="block w-full p-4 pl-16 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
                   value={fullUrl}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setFullUrl(e.target.value)
@@ -54,7 +56,7 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
                 />
                 <button
                   type="submit"
-                  className="absolute top-0 end-0 p-2.5 text-white text-sm font-medium h-full text-wrap bg-blue-700 rounded-lg border border-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-500"
+                  className="absolute top-0 right-0 p-2.5 text-white text-sm font-medium h-full bg-blue-700 rounded-lg border border-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-500"
                 >
                   Shorten URL
                 </button>
